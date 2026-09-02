319c25ae6d99d65e7b168af36675a3f16b186bb4
GuuStav0 <luizg600@gmail.com>
2026-09-02 08:58:46 -0300

    Adiciona Estrutura da Landing Page

---
+++ src/routes/Landing.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import '../styles/landing.css'
+
+export default function Landing(){
+    
+
+    return(
+        <>
+            <div className='bg-gradient'>
+                <header>
+
+                </header>
+                <main>
+
+                </main>
+                <footer>
+
+                </footer>
+            </div>
+        </>
+    )
+}
