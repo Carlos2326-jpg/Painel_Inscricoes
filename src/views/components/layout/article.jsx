50396bb5eac77b8b8d1afeaffdbad21c69a60960
Carlos2326-jpg <joaocarlosdutra2326@gmail.com>
2026-09-15 16:18:32 -0300

    feat: adiciona componente de layout Article

---
+++ src/views/components/layout/article.jsx
@@ -0,0 +1,18 @@
+import '../../_shared/style/layout/article.css'
+
+export default function article() {
+    return (
+        <article className='article-block'>
+            <section className='block-text'>
+                <h2 className='text title-h2'>
+                    Tecnologia que <span>conecta pessoas e ideias.</span>
+                </h2>
+
+                <p className='text text-rigth'>
+                    A Semana de Tecnologia reúne estudantes, profissionais e apaixonados por tecnologia em uma experiência dedicada ao aprendizado, à troca de conhecimentos e à inovação. Durante três dias, você poderá explorar novas áreas, desenvolver habilidades e conhecer diferentes perspectivas sobre o futuro da tecnologia.
+                </p>
+            </section>
+
+        </article>
+    );
+}
