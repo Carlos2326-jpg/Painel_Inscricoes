f99f56cd8c12a44211f360aa2d8cde999ef39dd6
Carlos2326-jpg <joaocarlosdutra2326@gmail.com>
2026-09-15 16:19:16 -0300

    feat: atualiza página inicial com novo layout

--- src/views/pages/home.jsx
+++ src/views/pages/home.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/layout/header";
+import Article from "../components/layout/article";
 import Footer from "../components/layout/footer";
 
 export default function Home() {
     return (
         <main>
             <Header />
+            <Article />
             <Footer />
         </main>
     );
